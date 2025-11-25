fb3e56b3277ad91ad889f6904d733101830ff1b3
Oleksandr Veselovskyi <val.oleksandr.veselovskyi@lampenwelt.de>
2025-11-26 00:35:36 +0200

    Update

--- js/listing/Facets.js
+++ js/listing/Facets.js
@@ -15,7 +15,7 @@ export class Facets {
         $(this.component)?.addEventListener('change', (event) => {
             const element = event.target;
             if (element.type === 'checkbox') {
-                this.onChange(element);
+                this.onOptionChange(element);
             }
         });
 
@@ -25,14 +25,45 @@ export class Facets {
                 toggle.classList.toggle('active');
             });
         });
+
+        $$(this.component + ' [data-facet-type=range]').forEach((range) => {
+            const applyButton = $('button', range);
+            const rangeInputs = $$('input', range);
+            const minInput = rangeInputs[0];
+            const maxInput = rangeInputs[1];
+
+            rangeInputs.forEach((input) => {
+                input.addEventListener('keypress', (event) => {
+                    if (event.key === 'Enter') {
+                        this.onRangeChange(range);
+                    }
+                });
+
+                input.addEventListener('input', (event) => {
+                    const toDisable =
+                        minInput.value === minInput.dataset['current'] &&
+                        maxInput.value === maxInput.dataset['current'];
+                    applyButton.toggleAttribute('disabled', toDisable);
+                });
+            });
+
+            applyButton.addEventListener('click', () => {
+                this.onRangeChange(range);
+            });
+        });
     }
 
-    onChange(item) {
-        const query = this.getUpdatedQuery(item);
+    onOptionChange(item) {
+        const query = this.getOptionUpdatedQuery(item);
         this.applyQuery(query);
     }
 
-    getUpdatedQuery(item) {
+    onRangeChange(item) {
+        const query = this.getRangeUpdatedQuery(item);
+        this.applyQuery(query);
+    }
+
+    getOptionUpdatedQuery(item) {
         const query = new URLSearchParams(location.search);
         let qValue = query.get(item.name) ?? '';
         qValue = qValue ? qValue.split('_') : [];
@@ -49,6 +80,21 @@ export class Facets {
             query.set(item.name, qValue.join('_'));
         } else {
             query.delete(item.name);
+        }
+
+        return query;
+    }
+
+    getRangeUpdatedQuery(item) {
+        let code = item.dataset.facetCode,
+            inputs = $$('input', item),
+            value = inputs[0].value + '-' + inputs[1].value;
+
+        const query = new URLSearchParams(location.search);
+        if (value === '-') {
+            query.delete(code);
+        } else {
+            query.set(code, value);
         }
 
         return query;
